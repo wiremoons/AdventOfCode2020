a90e1ab8ffd3765e9203559ed32c4b41c6a483fd
Simon Rowe <simon@wiremoons.com>
2022-04-19 09:22:22 +0100

    Completed solution for Day 1 Part 1

--- Day-01/aoc_day01_p1.cc
+++ Day-01/aoc_day01_p1.cc
@@ -18,19 +18,58 @@
 // g++ -std=c++20 -Wall -o aoc_day01_p1 aoc_day01_p1.cc
 //
 
+#include <algorithm>
+#include <chrono>
+#include <exception>
 #include <fstream>
 #include <iostream>
+#include <ostream>
+#include <string>
+#include <vector>
 
 // GLOBALS
 // set if debug output is needed
 bool debug{false};
 
-/// @brief Count the total number of light in the 'on' state
-/// @param light_status is a boolean grid (matrix) of lights to be checked
-/// @ return the total number of lights that are on (ie set to true)
-int totalLightsOn()
+/// @brief Compare the expenses records to find two that total '2020'
+/// @param expenses : the vector of expenses as integers
+/// @ return the two expenses integers identified multiplied together
+int calculateProduct(std::vector<int> &expenses)
 {
     int result{0};
+    std::cout << "\n\nFinding correct expenses records (slow) to total '2020'..." << std::endl;
+    for (size_t i{0}; i < expenses.size(); i++) {
+        for (size_t j{0}; j < expenses.size(); j++) {
+            if (expenses.at(i) + expenses.at(j) == 2020) {
+                std::cout << "Expense records: " << expenses.at(i) << " + " << expenses.at(j) << " = "
+                          << expenses.at(i) + expenses.at(j) << std::endl;
+                return expenses.at(i) * expenses.at(j);
+            }
+        }
+    }
+
+    return result;
+}
+
+/// @brief Compare the expenses records to find two that total '2020'
+/// @param expenses : the vector of expenses as integers
+/// @ return the two expenses integers identified multiplied together
+int calculateProductFast(std::vector<int> &expenses)
+{
+    int result{0};
+    std::cout << "\n\nFinding correct expenses records (fast) to total '2020'..." << std::endl;
+
+    // first sort the expenses list for the binary search
+    std::sort(expenses.begin(), expenses.end());
+
+    // loop through expenses records - find match using binary search
+    for (auto expense : expenses) {
+        int locate = 2020 - expense;
+        if ((locate > expense) and (std::binary_search(expenses.begin(), expenses.end(), locate))) {
+            std::cout << "Expense records: " << expense << " + " << locate << " = " << expense + locate << std::endl;
+            return expense * locate;
+        }
+    }
 
     return result;
 }
@@ -55,20 +94,45 @@ int main()
 
     // variables to hold input data
     std::string line{};
+    std::vector<int> expenses{0};
     int line_number{0};
-    int count_changes{0};
-    int lights_on{0};
+    int answer{0};
 
-    //  read each line delimited by '\n' into 'line' from the file
+    // read each line delimited by '\n' into 'line' from the file
+    // convert to an integer and add to the vector of int 'expenses'.
     while (std::getline(input_file, line)) {
+        try {
+            line_number++;
+            expenses.push_back(std::stoi(line));
+        } catch (std::exception &e) {
+            std::cerr << "ERROR: failed to convert to integer on line: " << line_number << std::endl;
+            std::cerr << "Exception thrown was: " << e.what() << std::endl;
+            std::exit(1);
+        }
         line_number++;
     }
 
-    std::cout << std::endl << "Advent Of Code 2020 :  Day 01 Part 01" << '\n' << '\n';
-    std::cout << "  » Number of switches analysed : '" << count_changes << "'" << '\n';
-    std::cout << "  » PUZZLE ANSWER: number of 'xxxx' is: " << lights_on << '\n' << std::endl;
+    auto t1 = std::chrono::high_resolution_clock::now();
+    answer = calculateProduct(expenses);
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto duration_slow = duration_cast<std::chrono::nanoseconds>(t2 - t1);
+    std::cout << "Answer (slow): " << answer << std::endl;
+    std::cout << "--> Time taken by function: " << duration_slow.count() << " nanoseconds" << std::endl;
 
-    // Part 1 Answer :
+    auto t3 = std::chrono::high_resolution_clock::now();
+    answer = calculateProductFast(expenses);
+    auto t4 = std::chrono::high_resolution_clock::now();
+    auto duration_fast = duration_cast<std::chrono::nanoseconds>(t4 - t3);
+    std::cout << "Answer (fast): " << answer << std::endl;
+    std::cout << "--> Time taken by function: " << duration_fast.count() << " nanoseconds" << std::endl;
+
+    std::cout << std::endl << "Advent Of Code 2020 :  Day 01 Part 01" << '\n' << '\n';
+    std::cout << "  » Number of expenses entries analysed : '" << line_number << "'" << '\n';
+    std::cout << "  » PUZZLE ANSWER: two entries that sum to 2020 multiplied together : " << answer << '\n'
+              << std::endl;
+
+    // Part 1 TEST RESULT: 514579
+    // Part 1 Answer : 567171
 
     input_file.close();
     std::exit(0);
